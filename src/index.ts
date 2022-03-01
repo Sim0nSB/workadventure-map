ab4eaa8a4a03b6f71c3eba7e1ba2ff746fa2bc2b
Simon <64868759+Sim0nSB@users.noreply.github.com>
2022-03-01 16:44:54 +0100

    add Url

--- src/index.ts
+++ src/index.ts
@@ -20,19 +20,4 @@ function closePopUp(){
         currentPopup.close();
         currentPopup = undefined;
     }
-
-
-//customUrl1
-WA.room.onEnterLayer('customUrl1').subscribe(() => {
-    const coWebsite = await WA.nav.openCoWebSite('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-    const coWebsiteWorkAdventure = await WA.nav.openCoWebSite('https://workadventu.re/', true, "", 50, 1, true, true);
-})
-
-WA.room.onLeaveLayer('customUrl1').subscribe(closeWebsite)
-
-function closeWebsite(){
-    coWebsite.close(); 
-}
- 
-
 }
